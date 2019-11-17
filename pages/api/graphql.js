4d99fb3c2b99ae71178162a5cf30aebfe7a8d543
Tien Pham <tiendev94@gmail.com>
2019-11-17 17:29:41 +0200

    apollo server

---
+++ pages/api/graphql.js
@@ -0,0 +1,28 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return "Hello there!"
+    }
+  }
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });

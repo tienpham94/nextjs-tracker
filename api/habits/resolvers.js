674733736e45c1ad6919b59924301274f2dc287b
Tien Pham <tiendev94@gmail.com>
2019-11-21 23:06:30 +0200

    update

--- api/habits/resolvers.js
+++ api/habits/resolvers.js
@@ -1,13 +1,14 @@
+import Habits from "./habits";
+
 export const habitsResolvers = {
   Query: {
     async habits() {
-      console.log("habits");
-      return [
-        {
-          _id: "some array",
-          name: "make my bed"
-        }
-      ];
+      try {
+        const habits = await Habits.find()
+        return habits;
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 };

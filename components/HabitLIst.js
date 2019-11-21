121d22010172e1eb9b6a2c2b2c87adac81a1d816
Tien Pham <tiendev94@gmail.com>
2019-11-21 22:45:36 +0200

    useQuery

--- components/HabitLIst.js
+++ components/HabitLIst.js
@@ -1,12 +1,30 @@
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
 import Habit from "./Habit";
 
-const HabitList = ({ habits }) => (
-  <section>
-    <h2>My Habits</h2>
-    {habits.map((h, index) => (
-      <Habit key={h} habit={h} index={index} />
-    ))}
-  </section>
-);
+const GET_HABITS = gql`
+  query getHabits {
+    habits {
+      _id
+      name
+    }
+  }
+`;
+
+const HabitList = () => {
+  const { data, loading } = useQuery(GET_HABITS);
+  if (loading) return <section />;
+
+  const { habits } = data;
+  return (
+    <section>
+      <h2>My Habits</h2>
+      {habits.map((h, index) => (
+        <Habit key={h._id} habit={h} index={index} />
+      ))}
+    </section>
+  );
+};
 
 export default HabitList;
